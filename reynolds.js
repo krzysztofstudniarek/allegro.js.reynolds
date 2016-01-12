bcd010384fde0a16d6c74360dce4cd09b5f065fd
Krzysztof Studniarek <krzysztof.studniarek@mbank.pl>
2016-01-12 09:46:37 +0100

    boid drawing implemented

--- reynolds.js
+++ reynolds.js
@@ -1,8 +1,14 @@
 
-var boids
+var boidSprite;
+
+var boids;
 
 function draw()
 {
+	boids.forEach(function(boid){
+		rotate_sprite(canvas,boidSprite,boid.x,boid.y,DEG(Math.atan2(boid.vy,boid.vx)));
+	});
+	
 }
 
 function update()
@@ -14,6 +20,7 @@ function main()
     enable_debug('debug');
     allegro_init_all("game_canvas", 640, 480);
     hide_mouse();
+	load_elements();
     ready(function(){
         loop(function(){
             clear_to_color(canvas,makecol(0,255,0));
@@ -24,3 +31,18 @@ function main()
     return 0;
 }
 END_OF_MAIN();
+
+function load_elements(){
+	
+	boidSprite = load_bitmap("boid.png");
+	
+	boids = new Set();
+	boids.add(
+		{
+			x : 100,
+			y : 100,
+			vx : 5,
+			vy : 5
+		}
+	);
+}
